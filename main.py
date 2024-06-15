4ce7bc3961d6cbcac88d6e04a9236a998eb07075
monkzzz <65474617+monkzzz@users.noreply.github.com>
2024-06-15 18:22:56 +0100

    Added Prompt Templates and Output Parsers

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-import json 
 import requests
 from newspaper import Article
-from langchain_openai import ChatOpenAI
-from langchain.schema import (
-    HumanMessage
-)
+
+from langchain_openai import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import field_validator
+from pydantic import BaseModel, Field
+from typing import List
+
 from dotenv import load_dotenv
 load_dotenv()
+
+class ArticleSummary(BaseModel):
+    title: str = Field(description="Title of the article")
+    summary: List[str] = Field(description="Bulleted list summary of the article")
+
+    # validating whether the generated summary has at least three lines
+    @field_validator('summary')
+    def has_three_or_more_lines(cls, list_of_lines):
+        if len(list_of_lines) < 3:
+            raise ValueError("Generated summary has less than three bullet points!")
+        return list_of_lines
+
+# set up output parser
+parser = PydanticOutputParser(pydantic_object=ArticleSummary)
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
@@ -36,8 +53,10 @@ try:
         article_title = article.title
         article_text = article.text
 
-        # prepare template for prompt
-        template = """You are a very good assistant that summarizes online articles into bulleted lists.
+        # create prompt template
+        # notice that we are specifying the "partial_variables" parameter
+        template = """
+        You are a very good assistant that summarizes online articles.
 
         Here's the article you want to summarize.
 
@@ -47,24 +66,35 @@ try:
         {article_text}
         ==================
 
-        Write a summary of the previous article.
+        {format_instructions}
         """
 
-        # format prompt
-        prompt = template.format(article_title=article.title, article_text=article.text)
+        prompt = PromptTemplate(
+            template=template,
+            input_variables=["article_title", "article_text"],
+            partial_variables={"format_instructions": parser.get_format_instructions()}
+        )
 
-        messages = [HumanMessage(content=prompt)]
+        # Format the prompt using the article title and text obtained from scraping
+        formatted_prompt = prompt.format_prompt(article_title=article_title, article_text=article_text)
 
-        # load the model
-        chat = ChatOpenAI(model_name="gpt-4", temperature=0)
+        # instantiate model class
+        model = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.0)
 
-        # generate summary
-        summary = chat.invoke(messages)
-        print("Summary:")
-        print(summary.content)
+        # Use the model to generate a summary
+        output = model.invoke(formatted_prompt.to_string())
+        print("Output:")
+        print(output)
+        print(" ")
+
+        # Parse the output into the Pydantic model
+        parsed_output = parser.parse(output)
+        print("Parsed Output:")
+        print(parsed_output)
         print(" ")
 
     else:
         print(f"Failed to fetch article at {article_urls}")
+
 except Exception as e:
     print(f"Error occurred while fetching article at {article_urls}: {e}")
